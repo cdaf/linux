7b0b70930de3d0cd4478952c3459f01c7f3fb3df
Jules Clements' <julesc@datacom.co.nz>
2016-03-31 14:35:18 +1300

    commit penind

--- automation/provisioning/dns.sh
+++ automation/provisioning/dns.sh
@@ -9,6 +9,6 @@ echo "dns.sh : buildnumber : $buildnumber"
 # Use hosts entries to provide DNS within the closed network
 echo "Add app.skynet and buildserver.skynet to each others hosts file"
 sudo sh -c 'echo "172.16.17.101 app.skynet" >> /etc/hosts'
-sudo sh -c 'echo "172.16.17.102 buildserver.skynet" >> /etc/hosts'
+sudo sh -c 'echo "172.16.17.106 buildserver.skynet" >> /etc/hosts'
  
 echo "dns.sh : --- end ---"

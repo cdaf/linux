dbf2ddd823830d0be2db1d68784bdb5e54a631c5
Jules Clements <cdaf@outlook.co.nz>
2016-11-22 11:02:44 +1300

    CDAF 1.4.2.13 support non system wide installation of Ansible using Python virtual environment.

--- automation/provisioning/installAnsible.sh
+++ automation/provisioning/installAnsible.sh
@@ -19,7 +19,11 @@ if [ -z "$systemWide" ]; then
 	systemWide='yes'
 	echo "[$scriptName]   systemWide   : $systemWide (default)"
 else
-	echo "[$scriptName]   systemWide   : $systemWide"
+	if [ "$systemWide" == 'yes' ] || [ "$systemWide" == 'no' ]; then
+		echo "[$scriptName]   systemWide   : $systemWide"
+	else
+		echo "[$scriptName] Expecting yes or no, exiting with error code 1"; exit 1
+	fi
 fi
 
 version=$2
@@ -27,7 +31,7 @@ if [ -z "$version" ]; then
 	echo "[$scriptName]   version      : Not supplied, will use default"
 else
 	echo "[$scriptName]   version      : $version"
-	version="-${version}"
+	export ansibleVersion="-${version}"
 fi
 
 centos=$(uname -mrs | grep .el)
@@ -45,32 +49,47 @@ fi
 if [ "$centos" ]; then # Fedora
 
 	echo "[$scriptName] TODO: Fedora not implemented."
+	# For non-system wide install
+	# yum install gcc openssl-devel libffi-devel python-devel
 
 else # Debian
 
 
 	if [ "$systemWide" == 'yes' ]; then
 		executeExpression "sudo apt-get install software-properties-common"
-		executeExpression "sudo apt-add-repository ppa:ansible/ansible${version} -y"
-		executeExpression "sudo apt-get update -y"
+		executeExpression "sudo apt-add-repository ppa:ansible/ansible${ansibleVersion} -y"
+		executeExpression "sudo apt-get update"
 		executeExpression "sudo apt-get install -y ansible"
+			
 	else
+		executeExpression "sudo apt-get update"
+		executeExpression "sudo apt-get install -y build-essential libssl-dev libffi-dev python-dev"
 		executeExpression "sudo pip install virtualenv virtualenvwrapper"
 		executeExpression "source `which virtualenvwrapper.sh`"
-		if [ ! -d ~/ansible${version} ]; then
-			executeExpression "mkdir ~/ansible${version}"
-			executeExpression "cd ~/ansible${version}"
-			executeExpression "mkvirtualenv ansible${version}"
+		if [ ! -d ~/ansible${ansibleVersion} ]; then
+			executeExpression "mkdir ~/ansible${ansibleVersion}"
+			executeExpression "cd ~/ansible${ansibleVersion}"
+			executeExpression "mkvirtualenv ansible${ansibleVersion}"
 		else
-			executeExpression "cd ~/ansible${version}"
+			executeExpression "cd ~/ansible${ansibleVersion}"
 		fi
-		executeExpression "workon ansible${version}"
-		executeExpression "pip install ansible"
+		executeExpression "workon ansible${ansibleVersion}"
+		if [ -z "$version" ]; then
+			executeExpression "pip install ansible$"
+		else
+			executeExpression "pip install ansible==${version}"
+		fi
 	fi
 
 fi
 
-echo "[$scriptName] List version details..."
-executeExpression "python${version} --version"
+test=$(ansible-playbook --version 2>&1)
+if [[ $test == *"not found"* ]]; then
+	echo "[$scriptName] Anisble playbook not installed!"; exit 99
+else
+	IFS=' ' read -ra ADDR <<< $test
+	test=${ADDR[1]}
+	echo "[$scriptName] Anisble playbook version : $test"
+fi	
  
 echo "[$scriptName] --- end ---"

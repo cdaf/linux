4ab88bede692f2427c5706d9d0ff5fba8d19eb12
Jules Clements <cdaf@outlook.co.nz>
2016-12-04 09:02:57 +1300

    Create a new Ant provisioning script to support passing media argument (existing ant.sh will be deprecated in future).

---
+++ automation/provisioning/installApacheAnt.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+function executeExpression {
+	echo "[$scriptName] $1"
+	eval $1
+	exitCode=$?
+	# Check execution normal, anything other than 0 is an exception
+	if [ "$exitCode" != "0" ]; then
+		echo "$0 : Exception! $EXECUTABLESCRIPT returned $exitCode"
+		exit $exitCode
+	fi
+}  
+scriptName='ant.sh'
+
+echo "[$scriptName] --- start ---"
+if [ -z "$1" ]; then
+	echo "version not passed, HALT!"
+	exit 1
+else
+	version="$1"
+	echo "[$scriptName]   version    : $version"
+fi
+
+mediaCache="$2"
+if [ -z "$mediaCache" ]; then
+	mediaCache='/vagrant/.provision'
+	echo "[$scriptName]   mediaCache : $mediaCache (default)"
+else
+	echo "[$scriptName]   mediaCache : $mediaCache"
+fi
+
+# Set parameters
+executeExpression "antVersion=\"apache-ant-${version}\""
+executeExpression "antSource=\"$antVersion-bin.tar.gz\""
+
+executeExpression "cp \"${mediaCache}/${antSource}\" ."
+executeExpression "tar -xf $antSource"
+executeExpression "sudo mv $antVersion /opt/"
+
+# Configure to directory on the default PATH
+executeExpression "sudo ln -s /opt/$antVersion/bin/ant /usr/bin/ant"
+
+# Set environment (user default) variable
+echo ANT_HOME=\"/opt/$antVersion\" > $scriptName
+chmod +x $scriptName
+sudo mv -v $scriptName /etc/profile.d/
+
+echo "[$scriptName] List start script contents ..."
+executeExpression "cat /etc/profile.d/$scriptName"
+
+echo "[$scriptName] --- end ---"

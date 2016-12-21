847d74ce862fb7b6a0d7350b1b841fe3e4782f28
Jules Clements <cdaf@outlook.co.nz>
2016-12-22 10:22:41 +1300

    CDAF 1.4.3.04 correction to PM2 version listing and introduce service add script.

--- automation/provisioning/installpm2.sh
+++ automation/provisioning/installpm2.sh
@@ -42,6 +42,6 @@ echo "[$scriptName] sudo npm install pm2@latest -g"
 sudo sh -c 'for startScript in $(find /etc/profile.d -type f -name *.sh); do . $startScript ;echo $startScript; done; npm -version; npm install pm2@latest -g'
 
 executeExpression "pm2 startup $SYSTEMINITDAEMON"
-executeExpression "pm2 pm2 --version"
+executeExpression "pm2 --version"
 
 echo "[$scriptName] --- end ---"

75997ce5ec7475316f73471e645e827bf71c9293
Jules Clements <cdaf@outlook.co.nz>
2016-12-23 10:14:13 +1300

    CDAF 1.4.4 RC2 add APT lock check

--- automation/provisioning/installPostGreSQL.sh
+++ automation/provisioning/installPostGreSQL.sh
@@ -43,32 +43,26 @@ else
 	echo "[$scriptName]   version  : $version ($install)"
 fi
 
+echo
 # Install from global repositories only supporting CentOS and Ubuntu
 echo "[$scriptName] Determine distribution"
 uname -a
 centos=$(uname -a | grep el)
 if [ -z "$centos" ]; then
 	echo "[$scriptName] Ubuntu/Debian, update repositories using apt-get"
-	echo "[$scriptName] sudo apt-get update"
 	echo
-	timeout=3
-	count=0
-	while [ ${count} -lt ${timeout} ]; do
-		sudo apt-get update
-		exitCode=$?
-		if [ "$exitCode" != "0" ]; then
-	   	    ((count++))
-			echo "[$scriptName] apt-get sources update failed with exit code $exitCode, retry ${count}/${timeout} "
-		else
-			count=${timeout}
-		fi
-	done
-	if [ "$exitCode" != "0" ]; then
-		echo "[$scriptName] apt-get sources failed to update after ${timeout} tries."
-		echo "[$scriptName] Exiting with error code ${exitCode}"
-		exit $exitCode
+	echo "[$scriptName] Check that APT is available"
+	dailyUpdate=$(ps -ef | grep  /usr/lib/apt/apt.systemd.daily | grep -v grep)
+	if [ -n "${dailyUpdate}" ]; then
+		echo
+		echo "[$scriptName] ${dailyUpdate}"
+		IFS=' ' read -ra ADDR <<< $dailyUpdate
+		echo
+		executeExpression "sudo kill -9 ${ADDR[1]}"
+		executeExpression "sleep 5"
 	fi
 	
+	executeExpression "sudo apt-get update"
 	executeExpression "sudo apt-get install -y $install"
 
 else

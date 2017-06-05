393e34fdbe7f6cdc8eddf2def63a2989085bf0bb
Jules Clements <cdaf@outlook.co.nz>
2017-06-05 14:28:09 +1200

    Reaplce ~ with $HOME

--- AtlasBase.sh
+++ AtlasBase.sh
@@ -29,14 +29,14 @@ else
 fi
 
 echo "[$scriptName] As Vagrant user, trust the public key"
-if [ -d "~/.ssh" ]; then
-	echo "[$scriptName] Directory ~/.ssh already exists"
+if [ -d "$HOME/.ssh" ]; then
+	echo "[$scriptName] Directory $HOME/.ssh already exists"
 else
-	executeExpression "mkdir ~/.ssh"
+	executeExpression "mkdir $HOME/.ssh"
 fi
-executeExpression "chmod 0700 ~/.ssh"
-executeExpression "echo 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ==' > ~/.ssh/authorized_keys"
-executeExpression "chmod 0600 ~/.ssh/authorized_keys"
+executeExpression "chmod 0700 $HOME/.ssh"
+executeExpression "echo 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ==' > $HOME/.ssh/authorized_keys"
+executeExpression "chmod 0600 $HOME/.ssh/authorized_keys"
 
 echo "[$scriptName] Configuration tweek"
 executeExpression "sudo sh -c \"echo \"UseDNS no\" >> /etc/ssh/sshd_config\""

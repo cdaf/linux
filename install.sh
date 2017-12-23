0c9ddb3a9ca76f0a2313ed5a4fb506199d556772
Jules <cdaf@outlook.co.nz>
2017-12-23 19:40:10 +1300

    Add an extraction script

---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+function executeExpression {
+	echo "[$scriptName] $1"
+	eval $1
+	exitCode=$?
+	# Check execution normal, anything other than 0 is an exception
+	if [ "$exitCode" != "0" ]; then
+		echo "$0 : Exception! $EXECUTABLESCRIPT returned $exitCode"
+		exit $exitCode
+	fi
+}
+
+function executeYumCheck {
+	counter=1
+	max=5
+	success='no'
+	while [ "$success" != 'yes' ]; do
+		echo "[$scriptName][$counter] $1"
+		eval $1
+		exitCode=$?
+		# Exit 0 and 100 are both success
+		if [ "$exitCode" == "100" ] || [ "$exitCode" == "0" ]; then
+			success='yes'
+		else
+			counter=$((counter + 1))
+			if [ "$counter" -le "$max" ]; then
+				echo "[$scriptName] Failed with exit code ${exitCode}! Retrying $counter of ${max}"
+			else
+				echo "[$scriptName] Failed with exit code ${exitCode}! Max retries (${max}) reached."
+				exit $exitCode
+			fi					 
+		fi
+	done
+}
+
+scriptName='install.sh'
+echo "[$scriptName] --- start ---"
+
+executeExpression "curl -O http://cdaf.io/static/app/downloads/LU-CDAF.tar.gz"
+executeExpression "tar -xzf LU-CDAF.tar.gz"
+executeExpression "./automation/remote/capabilities.sh"
+
+echo "[$scriptName] --- end ---"

569d09633d79ef302b20c6373e3aff88c0feb7ec
Jules <cdaf@outlook.co.nz>
2017-12-24 15:41:10 +1300

    Download CDAF framework

--- bootstrap-vsts.sh
+++ bootstrap-vsts.sh
@@ -13,8 +13,48 @@ function executeExpression {
 
 scriptName='bootstrap-vsts.sh'
 echo "[$scriptName] --- start ---"
+url="$1"
+if [ -z "$url" ]; then
+	echo "url not passed, HALT!"
+	exit 101
+else
+	echo "[$scriptName]   url            : $url"
+fi
 
-executeExpression "./automation/provisioning/installDocker.sh"         # Docker and Compose
-executeExpression "./automation/provisioning/installOracleJava.sh jdk" # Docker and Compose
+pat="$2"
+if [ -z "$pat" ]; then
+	echo "pat not passed, HALT!"
+	exit 102
+else
+	echo "[$scriptName]   pat            : $pat"
+fi
+
+pool="$3"
+if [ -z "$pool" ]; then
+	pool='Default'
+	echo "[$scriptName]   pool           : $pool (default)"
+else
+	echo "[$scriptName]   pool           : $pool"
+fi
+
+agentName="$4"
+if [ -z "$agentName" ]; then
+	agentName=$(hostname)
+	echo "[$scriptName]   agentName      : $agentName (default)"
+else
+	echo "[$scriptName]   agentName      : $agentName"
+fi
+
+echo
+echo "[$scriptName] Download CDAF"
+executeExpression "curl -O https://codeload.github.com/cdaf/linux/zip/master"
+executeExpression "unzip master"
+executeExpression "chmod -R +x ./linux-master"
+executeExpression "cd ./linux-master/"
+
+echo
+echo "[$scriptName] Create agent user and register"
+executeExpression "./automation/provisioning/addUser.sh vstsagent vstsagent yes" # VSTS Agent with sudoer access
+executeExpression "./automation/provisioning/installAgent.sh $url \$pat $pool $agentName"
 
 echo "[$scriptName] --- end ---"

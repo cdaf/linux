38b821f631a7e4045e103012a32eddcac2b49937
Jules <cdaf@outlook.co.nz>
2018-01-02 15:10:34 +1300

    Apply elevation for CentOS configuration

--- AtlasImage.sh
+++ AtlasImage.sh
@@ -16,6 +16,20 @@ echo
 echo "[$scriptName] Generic provisioning for Linux"
 echo
 echo "[$scriptName] --- start ---"
+hypervisor=$1
+if [ -n "$hypervisor" ]; then
+	echo "[$scriptName]   hypervisor   : $hypervisor"
+else
+	echo "[$scriptName] hypervisor not passed, exit 1"; exit 1
+fi
+
+if [ $(whoami) != 'root' ];then
+	elevate='sudo'
+	echo "[$scriptName]   whoami       : $(whoami)"
+else
+	echo "[$scriptName]   whoami       : $(whoami) (elevation not required)"
+fi
+
 centos=$(uname -mrs | grep .el)
 if [ "$centos" ]; then
 	echo "[$scriptName]   Fedora based : $(uname -mrs)"
@@ -28,76 +42,69 @@ else
 	fi
 fi
 
-hypervisor=$1
-if [ -n "$hypervisor" ]; then
-	echo "[$scriptName]   hypervisor   : $hypervisor"
-else
-	echo "[$scriptName] hypervisor not passed, exit 1"; exit 1
-fi
-
 if [ "$hypervisor" == 'hyperv' ]; then
 	if [ "$centos" ]; then
-		executeExpression "yum update -y"
+		executeExpression "$elevate yum update -y"
 		sed --in-place --expression='s/^Defaults\s*requiretty/# &/' /etc/sudoers
-		executeExpression "cat /etc/sudoers"
-	   	executeExpression "yum install -y hyperv-daemons cifs-utils"
-		executeExpression "systemctl daemon-reload"
-		executeExpression "systemctl enable hypervkvpd"
+		executeExpression "$elevate cat /etc/sudoers"
+	   	executeExpression "$elevate yum install -y hyperv-daemons cifs-utils"
+		executeExpression "$elevate systemctl daemon-reload"
+		executeExpression "$elevate systemctl enable hypervkvpd"
 	else # Ubuntu
 		echo;echo "[$scriptName] Ubuntu does not require extensions (from 14.04)"
 	fi
 else # VirtualBox
 	if [ "$centos" ]; then
-		executeExpression "sudo yum update -y"
+		executeExpression "$elevate yum update -y"
 		sed --in-place --expression='s/^Defaults\s*requiretty/# &/' /etc/sudoers
-		executeExpression "cat /etc/sudoers"
-		executeExpression "rpm -Uvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm"
-		executeExpression "yum install -y gcc kernel-devel kernel-headers dkms make bzip2 perl"
+		executeExpression "$elevate cat /etc/sudoers"
+		executeExpression "$elevate rpm -Uvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm"
+		executeExpression "$elevate yum install -y gcc kernel-devel kernel-headers dkms make bzip2 perl"
 		executeExpression "KERN_DIR=/usr/src/kernels/`uname -r`"
 		executeExpression "export KERN_DIR"
 	
 		echo;echo "[$scriptName] Load the Guest Editions using the VirtualBox GUI, Devices --> Insert Guest Editions ISO image"
-		executeExpression "mkdir /media/VirtualBoxGuestAdditions"
-		executeExpression "mount -r /dev/cdrom /media/VirtualBoxGuestAdditions"
+		executeExpression "$elevate mkdir /media/VirtualBoxGuestAdditions"
+		executeExpression "$elevate mount -r /dev/cdrom /media/VirtualBoxGuestAdditions"
 		executeExpression "cd /media/VirtualBoxGuestAdditions"
-		executeExpression "./VBoxLinuxAdditions.run"
+		executeExpression "$elevate ./VBoxLinuxAdditions.run"
 	else # Ubuntu
 		echo;echo "[$scriptName] Install prerequisites, then Download and install VirtualBox extensions"
 		vbadd='5.1.10'
-		executeExpression "sudo apt-get install -y linux-headers-$(uname -r) build-essential dkms"
+		executeExpression "$elevate apt-get install -y linux-headers-$(uname -r) build-essential dkms"
 		executeExpression "wget http://download.virtualbox.org/virtualbox/${vbadd}/VBoxGuestAdditions_${vbadd}.iso"
-		executeExpression "sudo mkdir /media/VBoxGuestAdditions"
-		executeExpression "sudo mount -o loop,ro VBoxGuestAdditions_${vbadd}.iso /media/VBoxGuestAdditions"
+		executeExpression "$elevate mkdir /media/VBoxGuestAdditions"
+		executeExpression "$elevate mount -o loop,ro VBoxGuestAdditions_${vbadd}.iso /media/VBoxGuestAdditions"
 			
 		# This is normal for server install
 		# Could not find the X.Org or XFree86 Window System, skipping.
-		executeExpression "sudo sh /media/VBoxGuestAdditions/VBoxLinuxAdditions.run"
+		executeExpression "$elevate sh /media/VBoxGuestAdditions/VBoxLinuxAdditions.run"
 		executeExpression "rm VBoxGuestAdditions_${vbadd}.iso"
-		executeExpression "sudo umount /media/VBoxGuestAdditions"
-		executeExpression "sudo rmdir /media/VBoxGuestAdditions"
+		executeExpression "$elevate umount /media/VBoxGuestAdditions"
+		executeExpression "$elevate rmdir /media/VBoxGuestAdditions"
 	fi
 
 fi
 
 if [ "$centos" ]; then
 	echo "[$scriptName] Cleanup"
-	executeExpression "yum clean all"
-	executeExpression "rm -rf /tmp/*"
-	executeExpression "rm -f /var/log/wtmp /var/log/btmp"
-	executeExpression "history -c"
-	executeExpression "dd if=/dev/zero of=/EMPTY bs=1M"
-	executeExpression "rm -f /EMPTY"
-	executeExpression "sync"
+	executeExpression "$elevate yum clean all"
+	executeExpression "$elevate rm -rf /tmp/*"
+	executeExpression "$elevate rm -f /var/log/wtmp /var/log/btmp"
+	executeExpression "$elevate history -c"
+	executeExpression "$elevate dd if=/dev/zero of=/EMPTY bs=1M"
+	executeExpression "$elevate rm -f /EMPTY"
+	executeExpression "$elevate sync"
 else # Ubuntu
-	executeExpression "sudo apt-get autoremove && sudo apt-get clean && sudo apt-get autoclean" 
-	executeExpression "sudo rm -r /var/log/*"
-	executeExpression "sudo telinit 1"
-	executeExpression "sudo mount -o remount,ro /dev/sda1"
-	executeExpression "sudo zerofree -v /dev/sda1" 
+	executeExpression "$elevate apt-get autoremove && $elevate apt-get clean && $elevate apt-get autoclean" 
+	executeExpression "$elevate rm -r /var/log/*"
+	executeExpression "$elevate telinit 1"
+	executeExpression "$elevate mount -o remount,ro /dev/sda1"
+	executeExpression "$elevate zerofree -v /dev/sda1" 
 fi
 
-echo "[$scriptName] Image complete, shutdown VM"
-executeExpression "shutdown -h 2"
+echo "[$scriptName] Image complete, shutdown VM with 1 minute pause"
+executeExpression "$elevate shutdown -h 1"
 
 echo "[$scriptName] --- end ---"
 exit 0

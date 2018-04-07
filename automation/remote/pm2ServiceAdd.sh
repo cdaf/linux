71909109f34928a5c785d74daedaf55d676dced9
jules <cdaf@outlook.co.nz>
2018-04-07 15:52:05 +1200

    1.7.2.03 rovide robust PM2 application updates

--- automation/remote/pm2ServiceAdd.sh
+++ automation/remote/pm2ServiceAdd.sh
@@ -1,32 +1,59 @@
 #!/usr/bin/env bash
+function executeExpression {
+	echo "[$scriptName] $1"
+	eval $1
+	exitCode=$?
+	# Check execution normal, anything other than 0 is an exception
+	if [ "$exitCode" != "0" ]; then
+		echo "$0 : Exception! $EXECUTABLESCRIPT returned $exitCode"
+		exit $exitCode
+	fi
+}  
+
 scriptName='pm2ServiceAdd.sh'
-echo
-echo "[$scriptName] : Add a nodejs process to pm2, usage: [$scriptName][workingDirectory][nodejsprocess]"
-echo "[$scriptName] : Example ./$scriptName /opt/gateway server.js"
-echo
 
-if [ "$#" -ne 2 ]; then
-  echo "[$scriptName] :ERROR: must specify directory and nodejs file"
+if [ "$#" -lt 2 ]; then
+	echo "[$scriptName] : Add a nodejs process to pm2, usage: $scriptName installDirectory entryPoint"
+	echo "[$scriptName] : Example ./$scriptName /opt/gateway server.js"
   exit 1
 fi
 
-workingDirectory="$1"
-nodejsProcess="$2"
+echo "[$scriptName] --- start ---"
+installDirectory="$1"
+echo "[$scriptName]   installDirectory : $installDirectory"
 
-if [ ! -f "${workingDirectory}/${nodejsProcess}" ]; then
-  echo "[$scriptName] :ERROR:nodejs file nonexistant [${workingDirectory}/${nodejsProcess}]"
-  exit 1
+entryPoint="$2"
+echo "[$scriptName]   entryPoint       : $entryPoint"
+
+name="$3"
+if [ -z "$name" ]; then
+	name="$entryPoint"
+	echo "[$scriptName]   name             : $name (defaulted to entryPoint)"
+else
+	echo "[$scriptName]   name             : $name"
 fi
 
-sh -c "cat <<'EOF' >${workingDirectory}/${nodejsProcess}.json
+if [ ! -f "${installDirectory}/${entryPoint}" ]; then
+  echo "[$scriptName] entryPoint file non-existant [${installDirectory}/${entryPoint}]"
+  exit 7785
+fi
+
+sh -c "cat <<'EOF' >${installDirectory}/${entryPoint}.json
 {
-\"name\":\"${nodejsProcess}\",
-\"script\":\"${workingDirectory}/${nodejsProcess}\",
-\"CWD\":\"${workingDirectory}\"
+\"name\":\"${name}\",
+\"script\":\"${installDirectory}/${entryPoint}\",
+\"cwd\":\"${installDirectory}\"
 }
 EOF"
 
-sh -c "pm2 start ${workingDirectory}/${nodejsProcess}.json --watch"
+echo
+executeExpression "cat ${installDirectory}/${entryPoint}.json"
+
+echo
+executeExpression "pm2 reload ${installDirectory}/${entryPoint}.json"
+
+echo
+executeExpression "pm2 start ${installDirectory}/${entryPoint}.json --watch"
 
 echo "[$scriptName] --- end ---"
 exit 0

0512e7c7cc2a697e477861bed5ed4d307ce8b8a7
Jules <cdaf@outlook.co.nz>
2018-09-04 13:37:16 +1200

    Provide portable agent bootstrap, i.e. vagrant, docker, cloud

--- samples/bootstrapAgent.sh
+++ samples/bootstrapAgent.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 function executeExpression {
 	counter=1
-	max=5
+	max=3
 	success='no'
 	while [ "$success" != 'yes' ]; do
 		echo "[$scriptName][$counter] $1"
@@ -24,22 +24,24 @@ function executeExpression {
 
 scriptName='bootstrapAgent.sh'
 
-echo "[$scriptName] --- start ---"
-echo "[$scriptName] Working directory is $(pwd)"
+echo; echo "[$scriptName] --- start ---"
 if [ $(whoami) != 'root' ];then
 	elevate='sudo'
-	echo "[$scriptName]   whoami  : $(whoami)"
+	echo "[$scriptName]  whoami : $(whoami)"
 else
-	echo "[$scriptName]   whoami  : $(whoami) (elevation not required)"
+	echo "[$scriptName]  whoami : $(whoami) (elevation not required)"
 fi
+echo "[$scriptName]  pwd    : $(pwd)"
 
 # First check for CDAF in current directory, then check for a Vagrant VM, if not Vagrant, default is to use the latest from GitHub (stable='no')
-if [ -d './automation/provisioning' ]; then
-	atomicPath='.'
+defaultPath='./automation'
+if [ -d "${defaultPath}/provisioning" ]; then
+	atomicPath="$defaultPath"
 else
-	echo "[$scriptName] Provisioning directory ($atomicPath) not found in workspace, looking for alternative ..."
-	if [ -d '/vagrant/automation' ]; then
-		atomicPath='/vagrant'
+	echo "[$scriptName] Provisioning directory ($defaultPath) not found in workspace, looking for alternative ..."
+	if [ -d '/vagrant/automation/provisioning' ]; then
+		atomicPath='/vagrant/automation'
+		echo "[$scriptName] Vagrant synchronised directory ($atomicPath) found"
 	else
 		if [[ $stable == 'no' ]]; then
 			echo "[$scriptName] $atomicPath not found for Vagrant, download latest from GitHub"
@@ -48,13 +50,13 @@ else
 			fi
 			executeExpression "curl -s https://codeload.github.com/cdaf/linux/zip/master --output linux-master.zip"
 			executeExpression "unzip linux-master.zip"
-			executeExpression "cd linux-master/"
+			atomicPath='./linux-master/automation'
 		else
 			echo "[$scriptName] $atomicPath not found for Vagrant, download latest from GitHub"
 			executeExpression "curl -s -O http://cdaf.io/static/app/downloads/LU-CDAF.tar.gz"
 			executeExpression "tar -xzf LU-CDAF.tar.gz"
+			atomicPath="$defaultPath"
 		fi
-		atomicPath='.'
 	fi
 fi
 
@@ -71,9 +73,9 @@ else
 fi	
 
 echo
-executeExpression "$elevate $atomicPath/automation/provisioning/installOracleJava.sh jdk"
-executeExpression "$elevate $atomicPath/automation/provisioning/installApacheMaven.sh"
-executeExpression "$atomicPath/automation/remote/capabilities.sh"
+executeExpression "$elevate $atomicPath/provisioning/installOracleJava.sh jdk"
+executeExpression "$elevate $atomicPath/provisioning/installApacheMaven.sh"
+executeExpression "$atomicPath/remote/capabilities.sh"
 
 echo
 echo "[$scriptName] --- end ---"

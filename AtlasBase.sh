0877f260bfd6027d41124106a8ba3260f0da0496
Jules <cdaf@outlook.co.nz>
2018-11-17 17:41:17 +1300

    Split provision to restart after kernel devel & headers install

---
+++ AtlasBase.sh
@@ -0,0 +1,145 @@
+#!/usr/bin/env bash
+scriptName='AtlasBasee.sh'
+imageLog='/VagrantBox.txt'
+
+function writeLog {
+	echo "[$scriptName][$(date)] $1"
+	sudo sh -c "echo '[$scriptName] $1' >> $imageLog"
+}
+
+function executeExpression {
+	writeLog "$1"
+	eval $1
+	exitCode=$?
+	# Check execution normal, anything other than 0 is an exception
+	if [ "$exitCode" != "0" ]; then
+		writeLog "$0 : Exception! $EXECUTABLESCRIPT returned $exitCode"
+		exit $exitCode
+	fi
+}  
+
+function executeIgnore {
+	writeLog "$1"
+	eval $1
+	exitCode=$?
+	# Check execution normal, warn if exception but do not fail
+	if [ "$exitCode" != "0" ]; then
+		writeLog "$0 : Warning! $EXECUTABLESCRIPT returned $exitCode"
+	fi
+}  
+
+echo; writeLog "--- start ---"
+hypervisor=$1
+if [ -n "$hypervisor" ]; then
+	if [ "$hypervisor" == 'virtualbox' ]; then
+		vbadd='5.2.22'
+		writeLog "  hypervisor   : $hypervisor (installing extension version ${vbadd})"
+	else
+		writeLog "  hypervisor   : $hypervisor"
+	fi
+else
+	writeLog "  hypervisor   : (not passed, extension install will not be attempted)"
+fi
+
+if [[ $(whoami) != 'vagrant' ]];then
+	writeLog "  HALT! Do Not Run as Root or any user other than vagrant, this will apply incorrect permission"; exit 773
+else
+	writeLog "  whoami       : $(whoami)"
+fi
+
+writeLog "As Vagrant user, trust the public key"
+if [ -d "$HOME/.ssh" ]; then
+	writeLog "Directory $HOME/.ssh already exists"
+else
+	executeExpression "mkdir $HOME/.ssh"
+fi
+executeExpression "chmod 0700 $HOME/.ssh"
+executeExpression "echo 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ==' > $HOME/.ssh/authorized_keys"
+executeExpression "chmod 0600 $HOME/.ssh/authorized_keys"
+
+test=$(sudo cat /etc/ssh/sshd_config | grep UseDNS | grep -v '#')
+if [ "$test" ]; then
+writeLog "Configuration tweek already applied"
+else
+	writeLog "Configuration tweek"
+	writeLog "  sudo sh -c 'echo \"UseDNS no\" >> /etc/ssh/sshd_config'"
+	sudo sh -c 'echo "UseDNS no" >> /etc/ssh/sshd_config'
+fi
+executeExpression "sudo cat /etc/ssh/sshd_config | grep Use"
+
+test=$(sudo cat /etc/sudoers | grep vagrant)
+if [ "$test" ]; then
+	writeLog "Vagrant sudo permissions already set"
+else
+	writeLog "Permission for Vagrant to perform provisioning"
+	writeLog "  sudo sh -c 'echo \"vagrant ALL=(ALL) NOPASSWD: ALL\" >> /etc/sudoers'"
+	sudo sh -c 'echo "vagrant ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers'
+fi
+executeExpression "sudo cat /etc/sudoers | grep PASS"
+
+test="`yum --version 2>&1`"
+if [[ "$test" == *"not found"* ]]; then
+	ubuntu=$(uname -a | grep ubuntu)
+	if [ "$ubuntu" ]; then
+		writeLog "  Debian based : $(uname -mrs)"
+	else
+		writeLog "  $(uname -a), Unknown distributation, exiting!"; exit 883
+	fi
+else
+	centos=$(cat /etc/redhat-release | grep CentOS)
+	if [ -z "$centos" ]; then
+		echo "[$scriptName] Red Hat Enterprise Linux"
+		rhel='yes'
+	else
+		echo "[$scriptName] CentOS Linux : $centos"
+	fi
+fi
+
+if [ "$hypervisor" == 'hyperv' ]; then
+	if [ "$centos" ]; then
+		executeExpression "sudo yum update -y"
+		sed --in-place --expression='s/^Defaults\s*requiretty/# &/' /etc/sudoers
+		executeExpression "sudo cat /etc/sudoers"
+	   	executeExpression "sudo yum install -y hyperv-daemons cifs-utils"
+		executeExpression "sudo systemctl daemon-reload"
+		executeExpression "sudo systemctl enable hypervkvpd"
+	else # Ubuntu, from https://oitibs.com/hyper-v-lis-on-ubuntu-16/
+		echo;writeLog "Ubuntu extensions are included (from 12.04), but require activation, list before and after"
+		executeExpression "sudo cat /etc/initramfs-tools/modules"
+		echo
+		executeExpression 'sudo sh -c "echo \"hv_vmbus\" >> /etc/initramfs-tools/modules"'
+		executeExpression 'sudo sh -c "echo \"hv_storvsc\" >> /etc/initramfs-tools/modules"'
+		executeExpression 'sudo sh -c "echo \"hv_blkvsc\" >> /etc/initramfs-tools/modules"'
+		executeExpression 'sudo sh -c "echo \"hv_netvsc\" >> /etc/initramfs-tools/modules"'
+		echo
+		executeExpression "sudo cat /etc/initramfs-tools/modules"
+		echo
+		executeExpression "sudo apt-get upgrade -y"
+		executeExpression "sudo apt-get install -y --install-recommends linux-cloud-tools-$(uname -r)"
+		executeExpression "sudo update-initramfs -u"
+	fi
+else
+	if [ "$hypervisor" == 'virtualbox' ]; then
+		if [ "$centos" ]; then
+			echo;writeLog "Install prerequisites"
+			executeExpression "sudo yum update -y"
+			sudo sed --in-place --expression='s/^Defaults\s*requiretty/# &/' /etc/sudoers
+			executeExpression "sudo cat /etc/sudoers"
+			executeExpression "sudo yum groupinstall -y 'Development Tools'"
+			executeExpression "sudo yum install -y gcc kernel-devel-$(uname -r) kernel-headers dkms make bzip2 perl"
+			executeExpression "KERN_DIR=/usr/src/kernels/$(uname -r)"
+			executeExpression "export KERN_DIR"
+			executeExpression "ls $KERN_DIR"
+		
+		else # Ubuntu
+			echo;writeLog "Install prerequisites"
+			executeExpression "sudo apt-get upgrade -y"
+			executeExpression "sudo apt-get install -y linux-headers-$(uname -r) build-essential dkms"
+		fi
+	fi
+fi
+
+executeExpression "sudo shutdown -r 1"
+		
+writeLog "--- end ---"
+exit 0

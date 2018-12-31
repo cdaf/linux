df2c86e385c3013303a5704eab5948f8faa78069
Jules <cdaf@outlook.co.nz>
2019-01-01 08:20:04 +1300

    Additional diagnostics if override task lookup fails

--- automation/local/localTasks.sh
+++ automation/local/localTasks.sh
@@ -7,21 +7,21 @@ function executeExpression {
 	exitCode=$?
 	# Check execution normal, anything other than 0 is an exception
 	if [ "$exitCode" != "0" ]; then
-		echo "$scriptName : Exception! $EXECUTABLESCRIPT returned $exitCode"
+		echo "[$scriptName] Exception! $EXECUTABLESCRIPT returned $exitCode"
 		exit $exitCode
 	fi
 }  
 
 echo
-echo "$scriptName : +--------------------------------+"
-echo "$scriptName : | Process Locally Executed Tasks |"
-echo "$scriptName : +--------------------------------+"; echo
+echo "[$scriptName] +--------------------------------+"
+echo "[$scriptName] | Process Locally Executed Tasks |"
+echo "[$scriptName] +--------------------------------+"; echo
 if [ -z "$1" ]; then
 	echo "$scriptName Environment Argument not passed. HALT!"
 	exit 1
 else
 	ENVIRONMENT=$1
-	echo "$scriptName :   ENVIRONMENT  : $ENVIRONMENT"
+	echo "[$scriptName]   ENVIRONMENT  : $ENVIRONMENT"
 fi
 
 if [ -z "$2" ]; then
@@ -29,41 +29,41 @@ if [ -z "$2" ]; then
 	exit 2
 else
 	BUILDNUMBER=$2
-	echo "$scriptName :   BUILDNUMBER  : $BUILDNUMBER"
+	echo "[$scriptName]   BUILDNUMBER  : $BUILDNUMBER"
 fi
 
 if [ -z "$3" ]; then
-	echo "$scriptName : Solution Name not supplied. HALT!"
+	echo "[$scriptName] Solution Name not supplied. HALT!"
 	exit 3
 else
 	SOLUTION=$3
-	echo "$scriptName :   SOLUTION     : $SOLUTION"
+	echo "[$scriptName]   SOLUTION     : $SOLUTION"
 fi
 
 WORKDIR=$4
 # If passed, change to the working directory, if not passed, execute in current directory
 if [ "$WORKDIR" ]; then
 	cd $WORKDIR
-	echo "$scriptName :   WORKDIR      : $WORKDIR"
+	echo "[$scriptName]   WORKDIR      : $WORKDIR"
 else
-	echo "$scriptName :   WORKDIR      : $(pwd) (not passed, using current dir)"
+	echo "[$scriptName]   WORKDIR      : $(pwd) (not passed, using current dir)"
 fi
 
 if [ -z "$5" ]; then
-	echo "$scriptName :   OPT_ARG      : (Optional task argument not supplied)"
+	echo "[$scriptName]   OPT_ARG      : (Optional task argument not supplied)"
 else
 	OPT_ARG=$5
-	echo "$scriptName :   OPT_ARG      : $OPT_ARG"
+	echo "[$scriptName]   OPT_ARG      : $OPT_ARG"
 fi
-echo "$scriptName :   whoami       : $(whoami)"
-echo "$scriptName :   hostname     : $(hostname)"
-echo "$scriptName :   pwd          : $(pwd)"
+echo "[$scriptName]   whoami       : $(whoami)"
+echo "[$scriptName]   hostname     : $(hostname)"
+echo "[$scriptName]   pwd          : $(pwd)"
 
 if [ -d "./propertiesForLocalTasks" ]; then
 
 	taskList=$(find ./propertiesForLocalTasks -name "$ENVIRONMENT*" )
 	if [ -n "$taskList" ]; then
-		echo; echo "$scriptName : Preparing to process targets : "; echo		 
+		echo; echo "[$scriptName] Preparing to process targets : "; echo		 
 		for LOCAL_TASK_TARGET in $taskList; do
 			echo "  ${LOCAL_TASK_TARGET##*/}"
 		done
@@ -71,32 +71,33 @@ if [ -d "./propertiesForLocalTasks" ]; then
 		for LOCAL_TASK_TARGET in $taskList; do
 			LOCAL_TASK_TARGET=${LOCAL_TASK_TARGET##*/}
 			echo
-			echo "$scriptName :   LOCAL_TASK_TARGET    : $LOCAL_TASK_TARGET"
+			echo "[$scriptName]   LOCAL_TASK_TARGET    : $LOCAL_TASK_TARGET"
 
 			scriptOverride=$(./getProperty.sh "propertiesForLocalTasks/$LOCAL_TASK_TARGET" "deployScriptOverride")
 			if [ "$scriptOverride" ]; then
-				echo "$scriptName :   deployScriptOverride : $scriptOverride"
+				echo "[$scriptName]   deployScriptOverride : $scriptOverride"
 			executeExpression "./$scriptOverride '$SOLUTION' '$BUILDNUMBER' '$LOCAL_TASK_TARGET'" 
 			else
 			
-				echo "$scriptName :   deployScriptOverride : (property not defined)"
+				echo "[$scriptName]   deployScriptOverride : (property not defined)"
 				taskOverride=$(./getProperty.sh "propertiesForLocalTasks/$LOCAL_TASK_TARGET" "deployTaskOverride")
 				if [ -z "$taskOverride" ]; then
 					taskOverride="./tasksRunLocal.tsk"
-					echo "$scriptName :   deployTaskOverride   : (property not defined, using $taskOverride)"
+					echo "[$scriptName]   deployTaskOverride   : (property not defined, using $taskOverride)"
 				else
-					echo "$scriptName :   deployTaskOverride   : $taskOverride"
+					echo "[$scriptName]   deployTaskOverride   : $taskOverride"
 				fi
 				for overrideTask in $taskOverride; do
-					echo; echo "$scriptName : Execute $overrideTask"
+					echo; echo "[$scriptName] Execute $overrideTask"
 					if [ ! -f "$overrideTask" ]; then
-						echo "$scriptName $overrideTask not found! Exit with code 3225"
+						echo "[$scriptName][ERROR] $overrideTask not found! List workspace contents and exit with code 3225"
+						ls -al
 						exit 3225
 					fi
 					./execute.sh "$SOLUTION" "$BUILDNUMBER" "$LOCAL_TASK_TARGET" "$overrideTask" "$OPT_ARG" 2>&1
 					exitCode=$?
 					if [ "$exitCode" != "0" ]; then
-						echo "$scriptName : ./execute.sh \"$SOLUTION\" \"$BUILDNUMBER\" \"$LOCAL_TASK_TARGET\" \"$overrideTask\" \"$OPT_ARG\" failed! Returned $exitCode"
+						echo "[$scriptName] ./execute.sh \"$SOLUTION\" \"$BUILDNUMBER\" \"$LOCAL_TASK_TARGET\" \"$overrideTask\" \"$OPT_ARG\" failed! Returned $exitCode"
 						exit $exitCode
 					fi
 				done
@@ -108,10 +109,10 @@ if [ -d "./propertiesForLocalTasks" ]; then
 	
 	else
 		echo
-		echo "$scriptName :   Properties directory ($workingDir/propertiesForLocalTasks) exists but contains no files, no action taken. Check that properties file exists with prefix of $ENVIRONMENT."
+		echo "[$scriptName]   Properties directory ($workingDir/propertiesForLocalTasks) exists but contains no files, no action taken. Check that properties file exists with prefix of $ENVIRONMENT."
 		
 	fi
 else
 	echo
-	echo "$scriptName :   Properties directory ($workingDir/propertiesForLocalTasks) not found, no action taken."
+	echo "[$scriptName]   Properties directory ($workingDir/propertiesForLocalTasks) not found, no action taken."
 fi
--- automation/remote/deploy.sh
+++ automation/remote/deploy.sh
@@ -5,7 +5,7 @@ scriptName=${0##*/}
 # Deploy script for running on the local host, all solution parameters are read from the manifest
 # deployment properties, i.e. the unique values for this deployment are read from target properties file. 
 if [ -z "$1" ]; then
-	echo "$scriptName : Deployment Target name not passed. HALT!"
+	echo "[$scriptName] Deployment Target name not passed. HALT!"
 	exit 1
 else
 	DEPLOY_TARGET=$1
@@ -18,72 +18,73 @@ WORKDIR=$2
 OPT_ARG=$3
 
 echo
-echo "$scriptName :   DEPLOY_TARGET        : $DEPLOY_TARGET"
+echo "[$scriptName]   DEPLOY_TARGET        : $DEPLOY_TARGET"
 # If passed, change to the working directory, if not passed, execute in current directory
 if [ "$WORKDIR" ]; then
 	cd $WORKDIR
-	echo "$scriptName :   WORKDIR              : $WORKDIR"
+	echo "[$scriptName]   WORKDIR              : $WORKDIR"
 else
-	echo "$scriptName :   WORKDIR              : $(pwd) (not passed, using current)"
+	echo "[$scriptName]   WORKDIR              : $(pwd) (not passed, using current)"
 fi
 
 # Load solution and build number from Manifest (created in package process)
 SOLUTION=$(./$LOCAL_DIR_DEFAULT/getProperty.sh "./manifest.txt" "SOLUTION")
-echo "$scriptName :   SOLUTION             : $SOLUTION"
+echo "[$scriptName]   SOLUTION             : $SOLUTION"
 BUILDNUMBER=$(./$LOCAL_DIR_DEFAULT/getProperty.sh "./manifest.txt" "BUILDNUMBER")
-echo "$scriptName :   BUILDNUMBER          : $BUILDNUMBER"
+echo "[$scriptName]   BUILDNUMBER          : $BUILDNUMBER"
 
 if [ "$OPT_ARG" ]; then
-	echo "$scriptName :   OPT_ARG              : $OPT_ARG"
+	echo "[$scriptName]   OPT_ARG              : $OPT_ARG"
 else
-	echo "$scriptName :   OPT_ARG              : (not passed)"
+	echo "[$scriptName]   OPT_ARG              : (not passed)"
 fi
 
-echo "$scriptName :   whoami               : $(whoami)"
-echo "$scriptName :   hostname             : $(hostname)"
+echo "[$scriptName]   whoami               : $(whoami)"
+echo "[$scriptName]   hostname             : $(hostname)"
 
 cdafVersion=$(./$LOCAL_DIR_DEFAULT/getProperty.sh "./$LOCAL_DIR_DEFAULT/CDAF.properties" "productVersion")
-echo "$scriptName :   CDAF Version         : $cdafVersion"
+echo "[$scriptName]   CDAF Version         : $cdafVersion"
 
 scriptOverride=$(./getProperty.sh "./$DEPLOY_TARGET" "deployScriptOverride")
 if [ "$scriptOverride" ]; then
 	if [ ! -f "./$scriptOverride" ]; then
-		echo "$scriptName : $scriptOverride not found!"
+		echo "[$scriptName] $scriptOverride not found!"
 		exit 127
 	fi	 	
-	echo "$scriptName :   deployScriptOverride : deployScriptOverride"  
-	printf "$scriptName :   Executing ==> "  
+	echo "[$scriptName]   deployScriptOverride : deployScriptOverride"  
+	printf "[$scriptName]   Executing ==> "  
 	overrideExecute="./$scriptOverride $SOLUTION $BUILDNUMBER $DEPLOY_TARGET"
 	echo
 	echo "$overrideExecute"
 	eval $overrideExecute
 	exitCode=$?
 	if [ "$exitCode" != "0" ]; then
-		echo "$scriptName : $scriptOverride failed! Returned $exitCode"
+		echo "[$scriptName] $scriptOverride failed! Returned $exitCode"
 		exit $exitCode
 	fi
 		
 else
 
-	echo "$scriptName :   deployScriptOverride : (not set)"  
+	echo "[$scriptName]   deployScriptOverride : (not set)"  
 	taskOverride=$(./getProperty.sh "./$DEPLOY_TARGET" "deployTaskOverride")
 	if [ "$taskOverride" ]; then
-		echo "$scriptName :   deployTaskOverride   : $taskOverride"
+		echo "[$scriptName]   deployTaskOverride   : $taskOverride"
 	else
 		taskOverride="tasksRunRemote.tsk"
-		echo "$scriptName :   deployTaskOverride   : tasksRunRemote.tsk (default)"
+		echo "[$scriptName]   deployTaskOverride   : tasksRunRemote.tsk (default)"
 	fi
 	for overrideTask in $taskOverride; do
 		if [ ! -f $overrideTask ]; then
-			echo "$scriptName $overrideTask not found! Exit with code 3226"
+			echo "[$scriptName][ERROR] $overrideTask not found! List workspace contents and exit with code 3226"
+			ls -al
 			exit 3226
 		fi
 		echo
-		echo "$scriptName : Starting deploy process ..."
+		echo "[$scriptName] Starting deploy process ..."
 		./execute.sh "$SOLUTION" "$BUILDNUMBER" "$DEPLOY_TARGET" "$overrideTask" "$OPT_ARG"
 		exitCode=$?
 		if [ "$exitCode" != "0" ]; then
-			echo "$scriptName : ./execute.sh $SOLUTION $BUILDNUMBER $DEPLOY_TARGET $overrideTask $OPT_ARG failed! Returned $exitCode"
+			echo "[$scriptName] ./execute.sh $SOLUTION $BUILDNUMBER $DEPLOY_TARGET $overrideTask $OPT_ARG failed! Returned $exitCode"
 			exit $exitCode
 		fi
 	done

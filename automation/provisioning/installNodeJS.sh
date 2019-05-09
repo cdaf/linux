b99b0e2fab106d860eb43be3952a004f6c8d589c
Jules <cdaf@outlook.co.nz>
2019-05-09 20:08:32 +1200

    1.9.0.02 update NodeJS to use nodesource.com for explicit versions

--- automation/provisioning/installNodeJS.sh
+++ automation/provisioning/installNodeJS.sh
@@ -66,9 +66,9 @@ echo
 echo "[$scriptName] --- start ---"
 version="$1"
 if [ -z "$version" ]; then
-	echo "[$scriptName]   version        : (not supplied, will install canonical)"
+	echo "[$scriptName]   version    : (not supplied, will install canonical)"
 else
-	echo "[$scriptName]   version        : $version"
+	echo "[$scriptName]   version    : $version"
 	systemWide=$2
 	if [ -z "$systemWide" ]; then
 		systemWide='yes'
@@ -83,7 +83,7 @@ else
 
 	mediaCache="$3"
 	if [ -z "$mediaCache" ]; then
-		mediaCache='/_provision'
+		mediaCache='/.provision'
 		echo "[$scriptName]   mediaCache : $mediaCache (default)"
 	else
 		echo "[$scriptName]   mediaCache : $mediaCache"
@@ -92,9 +92,9 @@ fi
 
 if [ $(whoami) != 'root' ];then
 	elevate='sudo'
-	echo "[$scriptName]   whoami         : $(whoami)"
+	echo "[$scriptName]   whoami     : $(whoami)"
 else
-	echo "[$scriptName]   whoami         : $(whoami) (elevation not required)"
+	echo "[$scriptName]   whoami     : $(whoami) (elevation not required)"
 fi
 
 test="`yum --version 2>&1`"
@@ -111,39 +111,39 @@ else
 fi
 echo
 
-if [ -z "$version" ]; then
-	if [ -z "$fedora" ]; then
-		echo "[$scriptName] Debian/Ubuntu, update repositories using apt-get"
+if [ -z "$fedora" ]; then
+	echo "[$scriptName] Debian/Ubuntu, update repositories using apt-get"
+	echo
+	echo "[$scriptName] Check that APT is available"
+	dailyUpdate=$(ps -ef | grep  /usr/lib/apt/apt.systemd.daily | grep -v grep)
+	if [ -n "${dailyUpdate}" ]; then
 		echo
-		echo "[$scriptName] Check that APT is available"
-		dailyUpdate=$(ps -ef | grep  /usr/lib/apt/apt.systemd.daily | grep -v grep)
-		if [ -n "${dailyUpdate}" ]; then
-			echo
-			echo "[$scriptName] ${dailyUpdate}"
-			IFS=' ' read -ra ADDR <<< $dailyUpdate
-			echo
-			executeExpression "$elevate kill -9 ${ADDR[1]}"
-			executeExpression "sleep 5"
-		fi	
-		
-		echo "[$scriptName] $elevate apt-get update"
+		echo "[$scriptName] ${dailyUpdate}"
+		IFS=' ' read -ra ADDR <<< $dailyUpdate
 		echo
-		timeout=3
-		count=0
-		while [ ${count} -lt ${timeout} ]; do
-			$elevate apt-get update
-			exitCode=$?
-			if [ "$exitCode" != "0" ]; then
-		   	    ((count++))
-				echo "[$scriptName] apt-get sources update failed with exit code $exitCode, retry ${count}/${timeout} "
-			else
-				count=${timeout}
-			fi
-		done
+		executeExpression "$elevate kill -9 ${ADDR[1]}"
+		executeExpression "sleep 5"
+	fi	
+	
+	echo "[$scriptName] $elevate apt-get update"
+	echo
+	timeout=3
+	count=0
+	while [ ${count} -lt ${timeout} ]; do
+		$elevate apt-get update
+		exitCode=$?
 		if [ "$exitCode" != "0" ]; then
-			echo "[$scriptName] apt-get sources failed to update after ${timeout} tries, will try with existing cache ..."
+	   	    ((count++))
+			echo "[$scriptName] apt-get sources update failed with exit code $exitCode, retry ${count}/${timeout} "
+		else
+			count=${timeout}
 		fi
-		echo
+	done
+	if [ "$exitCode" != "0" ]; then
+		echo "[$scriptName] apt-get sources failed to update after ${timeout} tries, will try with existing cache ..."
+	fi
+	echo
+	if [ -z "$version" ]; then
 		executeExpression "$elevate apt-get install -y nodejs npm curl"
 		
 		test="`node -v 2>&1`"
@@ -156,74 +156,85 @@ if [ -z "$version" ]; then
 				exit 939
 			fi
 		fi
-		
-
 	else
-		echo "[$scriptName] CentOS/RHEL, update repositories using yum"
-		executeYumCheck "$elevate yum check-update"
+		executeExpression "$elevate apt-get install -y curl"
+	fi
 
-		if [ "$systemWide" == 'yes' ]; then
-			
-			if [ -z "$centos" ]; then # Red Hat Enterprise Linux (RHEL)
-				echo "[$scriptName] Red Hat Enterprise Linux"
-			    executeIgnore "$elevate yum install -y http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm"
-			else
-				executeExpression "$elevate yum install -y epel-release"
-			fi
+else
+	echo "[$scriptName] CentOS/RHEL, update repositories using yum"
+	executeYumCheck "$elevate yum check-update"
+
+	if [ "$systemWide" == 'yes' ]; then
+		
+		if [ -z "$centos" ]; then # Red Hat Enterprise Linux (RHEL)
+			echo "[$scriptName] Red Hat Enterprise Linux"
+		    executeIgnore "$elevate yum install -y http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm"
+		else
+			executeExpression "$elevate yum install -y epel-release"
 		fi
-	
-		echo
+	fi
+
+	echo
+	if [ -z "$version" ]; then
 		executeExpression "$elevate yum install -y curl sudo gcc-c++ make"
 		echo;echo "[$scriptName] Aligning to Ubuntu 16.04 canonical version, i.e. v4"
 		executeExpression "curl --silent --location https://rpm.nodesource.com/setup_4.x | $elevate bash -"
 		executeExpression "$elevate yum install -y nodejs"
-
-	fi
-	echo; echo "[$scriptName] Verify Node Package Manager (NPM) version"
-	executeExpression "npm --version"
-
-else
-
-	runtime="node-v${version}-linux-x64"
-	mediaFullPath="${mediaCache}/${runtime}.tar.gz"
-	
-	# Check for media
-	if [ -f "$mediaFullPath" ]; then
-		echo "[$scriptName] Media found $mediaFullPath"
 	else
-		echo "[$scriptName] Media not found, attempting download"
-		if [ ! -d "$mediaCache" ]; then
-			executeExpression "sudo mkdir -p $mediaCache"
+		executeExpression "$elevate yum install -y curl"
+	fi
+
+fi
+
+if [ -n "$version" ]; then
+
+	test="`curl --version 2>&1`"
+	if [[ "$test" == *"not found"* ]]; then
+		echo; echo "[$scriptName] Install Error! curl verification failed."
+		exit 934
+	fi	
+	IFS=' ' read -ra ADDR <<< $test
+	test=${ADDR[1]}
+	echo "[$scriptName] curl : $test"
+	
+	if [ -z "$fedora" ]; then # Debian
+		echo
+		if [ -z $elevate ]; then
+			executeExpression "curl -sL https://deb.nodesource.com/setup_${version}.x | bash -"
+		else
+			executeExpression "curl -sL https://deb.nodesource.com/setup_${version}.x | $elevate -E bash -"
 		fi
-		executeExpression "sudo curl -s -o $mediaFullPath http://nodejs.org/dist/v${version}/node-v${version}-linux-x64.tar.gz"
+		executeExpression "$elevate apt-get install -y nodejs"
+	else # Red Hat
+		executeExpression "$elevate yum install -y gcc-c++ make"
+		if [ -z $elevate ]; then
+			executeExpression "curl -sL https://rpm.nodesource.com/setup_${version}.x | bash -"
+		else
+			executeExpression "curl -sL https://rpm.nodesource.com/setup_${version}.x | $elevate -E bash -"
+		fi
+		
 	fi
-	
-	if [ "$systemWide" == 'yes' ]; then
-	
-		cd $mediaCache
-		executeExpression "sudo tar -xzf node-v* -C /opt"
-	
-		# Set the environment settings (requires elevation), replace if existing
-		echo "[$scriptName] echo export PATH=\"/opt/${runtime}/bin:$PATH\" > nodejs.sh"
-		echo export PATH=\"/opt/${runtime}/bin:$PATH\" > nodejs.sh
-	
-		executeExpression "chmod +x nodejs.sh"
-		executeExpression "sudo mv -v nodejs.sh /etc/profile.d/"
-	
-		# Execute the script to set the variable 
-		executeExpression "source /etc/profile.d/nodejs.sh"
-	
+		
+	# NodeJS components
+	test=$(node --version 2>/dev/null)
+	if [ -z "$test" ]; then
+		echo "[$scriptName] NodeJS           : (not installed)"
 	else
+		echo "[$scriptName] NodeJS           : $test"
+	fi	
 	
-		executeExpression "curl https://raw.githubusercontent.com/creationix/nvm/v0.13.1/install.sh | bash"
-		executeExpression "source ~/.bash_profile"
-		executeExpression "nvm install v${version}"
-		executeExpression "nvm alias default v${version}"
-	fi
+	# Node Package Manager
+	test=$(npm -version 2>/dev/null)
+	if [ -z "$test" ]; then
+		echo "[$scriptName] NPM              : (not installed)"
+	else
+		echo "[$scriptName] NPM              : $test"
+	fi	
+
 fi
 
 echo; echo "[$scriptName] Verify Node version"
-
+executeExpression "npm --version"
 executeExpression "node --version"
 
 echo "[$scriptName] --- end ---"

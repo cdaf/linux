6a7bcd2fca9ba9c6cc217e158dac9ba3389535d1
julesc <julesc@devops-train04.dsldev.local>
2019-05-26 16:23:13 +1200

    Update default VSO agent version to 2.150.3

--- automation/provisioning/installAgent.sh
+++ automation/provisioning/installAgent.sh
@@ -60,7 +60,7 @@ if [ $(whoami) != 'root' ];then
 else
 	echo "[$scriptName]   whoami         : $(whoami) (elevation not required)"
 fi
-version='2.136.1'
+version='2.150.3'
 echo "[$scriptName]   version        : $version"
 
 media="vsts-agent-linux-x64-${version}.tar.gz"

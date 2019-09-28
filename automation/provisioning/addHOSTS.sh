3f7a05abb9a98b8c5fb4b8fa23a40635f83b5e1d
Jules <cdaf@outlook.co.nz>
2019-09-28 14:50:58 +1200

    Functional equivalence to Windows

--- automation/provisioning/addHOSTS.sh
+++ automation/provisioning/addHOSTS.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
+
+function executeExpression {
+	echo "[$scriptName] $1"
+	eval "$1"
+	exitCode=$?
+	# Check execution normal, anything other than 0 is an exception
+	if [ "$exitCode" != "0" ]; then
+		echo "$0 : Exception! $EXECUTABLESCRIPT returned $exitCode"
+		exit $exitCode
+	fi
+}  
+
 scriptName='addHOSTS.sh'
 
 echo "[$scriptName] : --- start ---"
@@ -16,8 +28,49 @@ else
 	echo "[$scriptName]   hostname : $hostname"
 fi
 
-echo "Use hosts entries to provide DNS override"
-echo "  sudo sh -c \"echo \"$ip $hostname\" >> /etc/hosts\""
-sudo sh -c "echo \"$ip $hostname\" >> /etc/hosts"
+if [ "$(whoami)" != 'root' ];then
+	elevate='sudo'
+	echo "[$scriptName]   whoami   : $(whoami)"
+else
+	echo "[$scriptName]   whoami   : $(whoami) (elevation not required)"
+fi
+
+hostsFile='/etc/hosts'
+echo "[$scriptName] Read HOSTS to memory then clear the file"
+fileInMemory=$(cat ${hostsFile}) # cat will read all lines, native READ will miss lines that don't have line-feed
+executeExpression "$elevate truncate -s 0 $hostsFile"
+
+while read -r line; do
+
+    if [[ $line =~ $hostname ]]; then
+    	if [[ $elevate == 'sudo' ]]; then
+			echo "  sudo sh -c \"echo \"$ip $hostname\" >> /etc/hosts\""
+			sudo sh -c "echo \"$ip $hostname\" >> /etc/hosts"
+		else
+			echo "  sh -c \"echo \"$ip $hostname\" >> /etc/hosts\""
+			sh -c "echo \"$ip $hostname\" >> /etc/hosts"
+		fi
+		replaced='yes'
+    else
+    	if [[ $elevate == 'sudo' ]]; then
+			echo "  sudo sh -c \"echo \"$line\" >> /etc/hosts\""
+			sudo sh -c "echo \"$line\" >> /etc/hosts"
+		else
+			echo "  sh -c \"echo \"$line\" >> /etc/hosts\""
+			sh -c "echo \"$line\" >> /etc/hosts"
+		fi
+    fi
+
+done < <(echo "$fileInMemory")
  
+if [[ ! $replaced == 'yes' ]]; then
+   	if [[ $elevate == 'sudo' ]]; then
+		echo "  sudo sh -c \"echo \"$ip $hostname\" >> /etc/hosts\""
+		sudo sh -c "echo \"$ip $hostname\" >> /etc/hosts"
+   	else
+		echo "  sh -c \"echo \"$ip $hostname\" >> /etc/hosts\""
+		sh -c "echo \"$ip $hostname\" >> /etc/hosts"
+   	fi
+fi
+	
 echo "[$scriptName] : --- end ---"

2af7757be74d746f758c6304cb8f2588e27269b7
Jules <cdaf@outlook.co.nz>
2020-04-05 18:22:10 +1200

    only test file for CR if exists

--- automation/buildandpackage/packageCopyArtefacts.sh
+++ automation/buildandpackage/packageCopyArtefacts.sh
@@ -12,9 +12,8 @@ DRIVER=$1
 WORK_DIR_DEFAULT=$2
 AUTOMATIONROOT=$3
 
-grep -q $'\r' $DRIVER && exitIfCR $DRIVER
-
 if [ -f  "$DRIVER" ]; then
+	grep -q $'\r' $DRIVER && exitIfCR $DRIVER
 	echo; echo "[$scriptName] Copy artifacts defined in $DRIVER"; echo
 	config=$(cat ${DRIVER}) # cat will read all lines, native READ will miss lines that done have line-feed
 	while read -r line; do

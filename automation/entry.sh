378f935083d068c2e2a3e3ecf8d3241232fc802d
Jules <cdaf@outlook.co.nz>
2020-07-17 19:34:29 +1200

    Derive solution root

--- automation/entry.sh
+++ automation/entry.sh
@@ -11,15 +11,13 @@ function executeExpression {
 }
 
 # Entry point for branch based targetless CD
-
 scriptName=${0##*/}
 
-echo
-echo "[$scriptName] ===================="
+echo; echo "[$scriptName] ===================="
 echo "[$scriptName] Targetless Branch CD"
 echo "[$scriptName] ===================="
 AUTOMATIONROOT="$( cd "$(dirname "$0")" ; pwd -P )"
-echo "[$scriptName]   AUTOMATIONROOT : $AUTOMATIONROOT"
+echo "[$scriptName]   AUTOMATIONROOT  : $AUTOMATIONROOT"
 export CDAF_AUTOMATION_ROOT=$AUTOMATIONROOT
 
 BUILDNUMBER="$1"
@@ -34,21 +32,35 @@ if [ -z $BUILDNUMBER ]; then
 		let "BUILDNUMBER=$BUILDNUMBER + 1"
 	fi
 	echo $BUILDNUMBER > ${HOME}/buildnumber.counter
-	echo "[$scriptName]   BUILDNUMBER : $BUILDNUMBER (not passed, using local counterfile ${HOME}/buildnumber.counter)"
+	echo "[$scriptName]   BUILDNUMBER  : $BUILDNUMBER (not passed, using local counterfile ${HOME}/buildnumber.counter)"
 else
-	echo "[$scriptName]   BUILDNUMBER : $BUILDNUMBER"
+	echo "[$scriptName]   BUILDNUMBER  : $BUILDNUMBER"
 fi
 
 BRANCH="$2"
 if [ -z $BRANCH ]; then
 	BRANCH='targetlesscd'
-	echo "[$scriptName]   BRANCH      : $BRANCH (not passed, set to default)"
+	echo "[$scriptName]   BRANCH       : $BRANCH (not passed, set to default)"
 else
-	echo "[$scriptName]   BRANCH      : $BRANCH"
+	echo "[$scriptName]   BRANCH       : $BRANCH"
 fi
 
 ACTION="$3"
-echo "[$scriptName]   ACTION      : $ACTION"
+echo "[$scriptName]   ACTION       : $ACTION"
+
+# Check for user defined solution folder, i.e. outside of automation root, if found override solution root
+printf "[$scriptName]   solutionRoot : "
+for directoryName in $(find . -maxdepth 1 -mindepth 1 -type d); do
+	if [ -f "$directoryName/CDAF.solution" ] && [ "$directoryName" != "$LOCAL_WORK_DIR" ] && [ "$directoryName" != "$REMOTE_WORK_DIR" ]; then
+		solutionRoot="$directoryName"
+	fi
+done
+if [ -z "$solutionRoot" ]; then
+	solutionRoot="$automationRoot/solution"
+	echo "$solutionRoot (default, project directory containing CDAF.solution not found)"
+else
+	echo "$solutionRoot (override $solutionRoot/CDAF.solution found)"
+fi
 
 executeExpression "$AUTOMATIONROOT/processor/buildPackage.sh $BUILDNUMBER $BRANCH $ACTION"
 

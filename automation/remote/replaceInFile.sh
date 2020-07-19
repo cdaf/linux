8a914de4ae0736f751baa3b465f53bc5d81ff7bd
Jules <cdaf@outlook.co.nz>
2020-07-19 15:39:00 +1200

    Support for line delete based on string

--- automation/remote/replaceInFile.sh
+++ automation/remote/replaceInFile.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 scriptName=${0##*/}
 
+echo; echo "[$scriptName] --- start ---"
 # the file to be processed
 filename=$1
 if [ -z "$filename" ]; then
@@ -9,18 +10,23 @@ else
 	if [ ! -f $filename ]; then
 		echo "[$scriptName] No existing file, ${filename}"; exit 111
 	fi
+	echo "[$scriptName] logFile : $logFile"
 fi
 
 # the string to be replaced
 name=$2
 if [ -z "$name" ]; then
 	echo "[$scriptName] existing string not supplied"; exit 120
+else
+	echo "[$scriptName] name    : $name"
 fi
 
 # the new value
 value=$3
 if [ -z "$value" ]; then
-	echo "[$scriptName] new string not supplied"; exit 130
+	echo "[$scriptName] new string not supplied, existing lines will be deleted"
+else
+	echo "[$scriptName] value   : $value"
 fi
 
 # perform diff listing after
@@ -28,34 +34,45 @@ diff=$4
 if [ -z "$diff" ]; then
 	echo "[$scriptName] difference listing not required (choices yes or common)"
 else
-	echo "[$scriptName] difference listing requested (choices yes or common)"
+	echo "[$scriptName] diff    : $diff (choices yes or common)"
 	cp -f "${filename}" "/tmp/$(basename ${filename})"
 fi
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-	sed -i '' "s^${name}^${value}^g" ${filename}
-	exitCode=$?
-	# Check execution normal, anything other than 0 is an exception
-	if [ "$exitCode" != "0" ]; then
-		echo "[$scriptName] String replace in file failed, returned $exitCode"
-		echo "[$scriptName] sed -i '' \"s^${name}^${value}^g\" ${filename}"
-		exit $exitCode
+	if [ -z "$value" ]; then
+		sed -i '' "/${name}/d" ${filename}
+	else
+		sed -i '' "s^${name}^${value}^g" ${filename}
+		exitCode=$?
+		# Check execution normal, anything other than 0 is an exception
+		if [ "$exitCode" != "0" ]; then
+			echo "[$scriptName] String replace in file failed, returned $exitCode"
+			echo "[$scriptName] sed -i '' \"s^${name}^${value}^g\" ${filename}"
+			exit $exitCode
+		fi
 	fi
 else
-	sed -i "s^${name}^${value}^g" ${filename}
-	exitCode=$?
-	# Check execution normal, anything other than 0 is an exception
-	if [ "$exitCode" != "0" ]; then
-		echo "[$scriptName] String replace in file failed, returned $exitCode"
-		echo "[$scriptName] sed -i \"s^${name}^${value}^g\" ${filename}"
-		exit $exitCode
+	if [ -z "$value" ]; then
+		sed -i "/${name}/d" ${filename}
+	else
+		sed -i "s^${name}^${value}^g" ${filename}
+		exitCode=$?
+		# Check execution normal, anything other than 0 is an exception
+		if [ "$exitCode" != "0" ]; then
+			echo "[$scriptName] String replace in file failed, returned $exitCode"
+			echo "[$scriptName] sed -i \"s^${name}^${value}^g\" ${filename}"
+			exit $exitCode
+		fi
 	fi
 fi
 
-if [ "$diff" == 'yes' ]; then
-	diff --side-by-side --suppress-common-lines "/tmp/$(basename ${filename})" "${filename}"
-else
-	diff --side-by-side "/tmp/$(basename ${filename})" "${filename}"
+if [ ! -z "$diff" ]; then
+	if [ "$diff" == 'yes' ]; then
+		diff --side-by-side --suppress-common-lines "/tmp/$(basename ${filename})" "${filename}"
+	else
+		diff --side-by-side "/tmp/$(basename ${filename})" "${filename}"
+	fi
 fi
 
+echo; echo "[$scriptName] --- Finish ---"
 exit 0

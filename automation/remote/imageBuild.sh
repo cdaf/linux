8bcef5f4361a1f3eebfab910630df1d3d1fcc88a
Jules <cdaf@outlook.co.nz>
2020-08-28 09:56:08 +1200

    Clean after build, ensure only current image available

--- automation/remote/imageBuild.sh
+++ automation/remote/imageBuild.sh
@@ -14,43 +14,33 @@ scriptName='imageBuild.sh'
 
 echo; echo "[$scriptName] --- start ---"
 id=$1
-echo "[$scriptName]  id             : $id"
+echo "[$scriptName]  id              : $id"
 
 BUILDNUMBER=$2
-echo "[$scriptName]  BUILDNUMBER    : $BUILDNUMBER"
+echo "[$scriptName]  BUILDNUMBER     : $BUILDNUMBER"
 
 containerImage=$3
-echo "[$scriptName]  containerImage : $containerImage"
+echo "[$scriptName]  containerImage  : $containerImage"
 
 constructor=$4
 if [ -z "$constructor" ]; then
-	echo "[$scriptName]  constructor    : (not supplied, supports space separated list)"
+	echo "[$scriptName]  constructor     : (not supplied, supports space separated list)"
 else
-	echo "[$scriptName]  constructor    : $constructor (supports space separated list)"
+	echo "[$scriptName]  constructor     : $constructor (supports space separated list)"
 fi
 
 AUTOMATIONROOT=$CDAF_AUTOMATION_ROOT
 if [ -z "$AUTOMATIONROOT" ]; then
 	AUTOMATIONROOT='../automation'
-	echo "[$scriptName]  AUTOMATIONROOT : $AUTOMATIONROOT (not supplied, use relative path)"
+	echo "[$scriptName]  AUTOMATIONROOT  : $AUTOMATIONROOT (not supplied, use relative path)"
 else
-	echo "[$scriptName]  AUTOMATIONROOT : $AUTOMATIONROOT"
+	echo "[$scriptName]  AUTOMATIONROOT  : $AUTOMATIONROOT"
 fi
+
+echo "[$scriptName]  CONTAINER_IMAGE : $CONTAINER_IMAGE"
 
 workspace=$(pwd)
-echo "[$scriptName]  pwd            : $workspace"
-
-if [ -z "$containerImage" ]; then
-	echo "[$scriptName] containerImage  : (not defined in $solutionRoot/CDAF.solution)"
-else
-	if [ -z $CONTAINER_IMAGE ]; then
-		export CONTAINER_IMAGE="$containerImage"
-		echo "[$scriptName] CONTAINER_IMAGE : $CONTAINER_IMAGE (set to \$containerImage)"
-	else
-		echo "[$scriptName] containerImage  : $containerImage"
-		echo "[$scriptName] CONTAINER_IMAGE : $CONTAINER_IMAGE (not changed as already set)"
-	fi
-fi
+echo "[$scriptName]  pwd             : $workspace"
 
 echo "Create the image file system locally"
 if [ ! -d "/tmp/buildImage/${id}" ]; then
@@ -68,12 +58,14 @@ for image in $constructor; do
 	echo "------------------------"; echo
 	executeExpression "rm -rf /tmp/buildImage/${id}/**"
 	executeExpression "cp $AUTOMATIONROOT/remote/dockerBuild.sh /tmp/buildImage/${id}"
+	executeExpression "cp $AUTOMATIONROOT/remote/dockerClean.sh /tmp/buildImage/${id}"
 	executeExpression "cp -r $AUTOMATIONROOT /tmp/buildImage/${id}"
 	executeExpression "cp -r ${image}/** /tmp/buildImage/${id}"
 	executeExpression "cd /tmp/buildImage/${id}"
 	executeExpression "cat Dockerfile"
     image=$(echo "$image" | tr '[:upper:]' '[:lower:]')
-	executeExpression "./dockerBuild.sh ${id}_${image##*/} $BUILDNUMBER"
+	executeExpression "./dockerBuild.sh ${id}_${image##*/} $BUILDNUMBER $BUILDNUMBER no $(whoami) $(id -u)"
+	executeExpression "./dockerClean.sh ${id}_${image##*/} $BUILDNUMBER"
 	executeExpression "cd $workspace"
 
 done

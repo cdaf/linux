321bc9161e0405de40969d6a9aaf28f378c4c2fd
Jules Clements <cdaf@outlook.co.nz>
2023-03-05 20:36:03 +1300

    Remove quote wrap for list processing

--- automation/remote/imageBuild.sh
+++ automation/remote/imageBuild.sh
@@ -183,7 +183,7 @@ else
 			echo "Do not set CDAF_REGISTRY_URL when pushing to dockerhub"
 		else
 			executeExpression "echo $CDAF_REGISTRY_TOKEN | docker login --username $CDAF_REGISTRY_USER --password-stdin $registryURL"
-			for registryTag in "${CDAF_REGISTRY_TAG}"; do
+			for registryTag in ${CDAF_REGISTRY_TAG}; do
 				executeExpression "docker tag ${id}_${image##*/}:$BUILDNUMBER ${registryTag}"
 				executeExpression "docker push ${registryTag}"
 			done

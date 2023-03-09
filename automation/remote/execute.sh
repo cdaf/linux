c0670c061b97fb46b39428d1dd7df0b10ac8f361
Jules Clements <cdaf@outlook.co.nz>
2023-03-10 05:52:51 +1300

    Try a different single byte character that is not likely in a password

--- automation/remote/execute.sh
+++ automation/remote/execute.sh
@@ -61,7 +61,7 @@ function executeExpression {
 	if [ "$exitCode" != "0" ]; then
 		ERRMSG "$EXECUTABLESCRIPT returned $exitCode" $exitCode
 	fi
-}  
+}
 
 function MAKDIR {
 	# Create Directory, if exists do nothing, else create
@@ -86,7 +86,7 @@ function REMOVE {
 	if [ "$exitCode" != "0" ]; then
 		ERRMSG "Exception! ${executeFunction} returned $exitCode" $exitCode
 	fi
-}  
+}
 
 # Verbose Copy
 function VECOPY {
@@ -110,7 +110,7 @@ function VECOPY {
 			ERRMSG "${executeFunction} returned $exitCode" $exitCode
 		fi
 	fi
-}  
+}
 
 # Refresh Directory Contents
 # If single argument clear directory, if two arguments, copy source to clean destination
@@ -154,7 +154,7 @@ function REFRSH {
 			fi
 		done
 	fi
-}  
+}
 
 # Decrypt a file
 #  required : file to descrypt
@@ -165,7 +165,7 @@ function DECRYP {
 	if [ "$exitCode" != "0" ]; then
 		ERRMSG "$0 : Exception! decryptKey.sh $1 $2 returned $exitCode" $exitCode
 	fi
-}  
+}
 
 # Detokenise a file
 #  required : file to be detokenised
@@ -207,11 +207,11 @@ function REPLAC {
 	plaintext="$4"
 	# Mac OSX sed 
 	if [[ "$OSTYPE" == "darwin"* ]]; then
-		executeFunction="sed -i '' -- \"s•${token}•${value}•g\" ${fileName}"
+		executeFunction="sed -i '' -- \"sÂ»${token}Â»${value}Â»g\" ${fileName}"
 	else
-		executeFunction="sed -i -- \"s•${token}•${value}•g\" ${fileName}"
+		executeFunction="sed -i -- \"sÂ»${token}Â»${value}Â»g\" ${fileName}"
 	fi
-	printable=$(echo "${executeFunction//•/â€¢}")
+	printable=$(echo "${executeFunction//ï¿½/â€¢}")
 	if [ -z "$4" ]; then
 		echo "${printable//${value}/*****}"
 	else
@@ -262,10 +262,18 @@ function VARCHK {
 	if [ -z "$propertiesFile" ]; then
 		propertiesFile='properties.varchk'
 		echo "  VARCHK using $propertiesFile (default)"
+		if [ -f "$propertiesFile" ]; then
+			echo "  VARCHK using $propertiesFile (workspace default)"
+		else
+			propertiesFile="${SOLUTIONROOT}/properties.varchk"
+			if [ -f "$propertiesFile" ]; then
+				echo "  VARCHK using $propertiesFile (solution default)"
+			fi
+		fi
 	else
 		echo "  VARCHK using $propertiesFile"
 	fi
-	
+
 	if [ ! -f "$propertiesFile" ]; then
 		ERRMSG "[VARCHK_PROP_FILE_NOT_FOUND] $propertiesFile not found" 7781
 	fi

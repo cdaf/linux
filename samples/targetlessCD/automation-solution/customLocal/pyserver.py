72045b3406f2364779d3c2ae6702879c8a968156
Jules Clements <cdaf@outlook.co.nz>
2023-05-06 16:38:02 +1200

    rework without volume mount

---
+++ samples/targetlessCD/automation-solution/customLocal/pyserver.py
@@ -0,0 +1,11 @@
+import os
+from http.server import HTTPServer, CGIHTTPRequestHandler
+
+# Make sure the server is created at current directory
+os.chdir('.')
+
+# Create server object listening the port 8000
+server_object = HTTPServer(server_address=('0.0.0.0', 8000), RequestHandlerClass=CGIHTTPRequestHandler)
+
+# Start the web server
+server_object.serve_forever()
---
+++ samples/targetlessCD/readme.md
@@ -0,0 +1,9 @@
+# Targetless CD
+
+Execute function test without deploying a target, instead, perform test in docker-compose environment.
+
+## imageBuild
+
+Used to create the target image to be tested
+
+

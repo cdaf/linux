b7550d613bf806669a03661e642fbdf7cd5359b7
Jules <cdaf@outlook.co.nz>
2023-08-21 20:13:17 +1200

    Deprecate executeRetry

--- automation/remote/execute.sh
+++ automation/remote/execute.sh
@@ -63,6 +63,39 @@ function executeExpression {
 	fi
 }
 
+function EXERTY {
+	wait="$2"
+	if [ -z "$wait" ]; then
+		wait=10
+	fi
+
+	retryMax="$3"
+	if [ -z "$retryMax" ]; then
+		retryMax=5
+	fi
+
+	counter=1
+	success='no'
+	while [ "$success" != 'yes' ]; do
+		echo "[$scriptName][$counter] $1"
+		eval $1
+		exitCode=$?
+		# Check execution normal, anything other than 0 is an exception
+		if [ "$exitCode" != "0" ]; then
+			counter=$((counter + 1))
+			if [ "$counter" -le "$retryMax" ]; then
+				echo "[$scriptName] Failed with exit code ${exitCode}! Wait $wait seconds, then retry $counter of ${retryMax}"
+				sleep $wait
+			else
+				echo "[$scriptName] Failed with exit code ${exitCode}! Maximum retries (${retryMax}) reached."
+				exit $exitCode
+			fi
+		else
+			success='yes'
+		fi
+	done
+}
+
 function MAKDIR {
 	# Create Directory, if exists do nothing, else create
 	#  required : directory name
